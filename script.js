58c7880e6fbb42cdfe87fe9513e54ab8ac020043
Junhao Li <junhaoli2019@u.northwestern.edu>
2017-02-06 17:45:01 -0600

    Uploading all the files from Sarah EOM

---
+++ script.js
@@ -0,0 +1,5 @@
+var appliance_divs = document.getElementsByClassName('appliance_div');
+
+//loops thru each button
+//if (appliance_btn).onlick -- insert image of (appliance_popup) -- if click outside of popup or on X -- remove image of (appliance_popup)
+
