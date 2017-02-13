e9cb9482c9cbeb6af492ffbcc24bdfdeac60f22b
JosephEligon <JosephEligon@gmail.com>
2017-02-12 21:34:23 -0600

    Added icons bar and changed to how icons work.

--- js/script.js
+++ js/script.js
@@ -29,10 +29,17 @@ var close_prompt = function() {
 
 var add_icon = function(){
 	// add an icon to the bottom of the page for the chosen appliance
-	$('#' + chosen_app + '_icon').append('<img class="app_icons" src="icons/' + chosen_app + '_ico.png" alt="test.jpg"/>');
-	$('#' + chosen_app + '_icon').click(function(){
-		//prompt to remove single clicked icon
-	})
+	//$('#' + chosen_app + '_icon').append('<img class="app_icon" src="icons/' + chosen_app + '_ico.png" alt="test.jpg"/>'); // places icon at the bottom of the page
+	$('.app_icons').append('<img class="app_icon" id="' + chosen_app + '_icon" src="icons/' + chosen_app + '_ico.png" alt="test.jpg"/>'); //places icon at the bottom of the page
+	
+	// open a prompt asking if they want to remove (all?) appliances of a certain type on click
+	/*$('#' + chosen_app + '_icon').click(function(){
+		//prompt to remove single clicked icons
+		jConfirm('Can you confirm this?', 'Confirmation Dialog', function(r) {
+    	jAlert('Confirmed: ' + r, 'Confirmation Results');
+		});
+
+	})*/
 }
 
 // once document is loaded...
@@ -59,7 +66,9 @@ $(document).ready(function() {
 	// if 'OK' button is clicked, close prompt and make new icon appear
 	// icon feature is WIP
 	$('.ok').click(function() {
-		add_icon();
+		if($('.prompt_pic').hasClass('shadowed')){ //only if an appliance is selected, add the icon
+			add_icon();
+		}
 		close_prompt();
 	});
 
